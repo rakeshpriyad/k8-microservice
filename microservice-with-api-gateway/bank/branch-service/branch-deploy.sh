5c37712c2879675cba623fd318a7d863a46429e4
rakeshpriyad <rakeshpriyad@gmail.com>
2020-05-16 13:49:10 +0530

    commit

--- microservice-with-api-gateway/bank/branch-service/branch-deploy.sh
+++ microservice-with-api-gateway/bank/branch-service/branch-deploy.sh
@@ -5,18 +5,5 @@ cd ${B_SERVICE}
 docker build -t rkp/branch-service:1.0 .
 cd ${B_SERVICE}/src/main/resources
 
-#a. Deploy configmap 
-kubectl apply -f branch-mysql-configmap.yml
-#kubectl get configmap
-#b. Create credentials
-kubectl apply -f branch-mysqldb-credentials.yml
-kubectl apply -f branch-mysqldb-root-credentials.yml
-
-#kubectl get secrets
-
-#c. Deploy mysql
-kubectl apply -f branch-mysql-deployment.yml
-#kubectl describe pods <pod id>
-#kubectl get deployments
 #d. deploy branch service
 kubectl apply -f branch-service-deployment.yml
--- microservice-with-api-gateway/bank/customer-service/customer-deploy.sh
+++ microservice-with-api-gateway/bank/customer-service/customer-deploy.sh
@@ -8,17 +8,6 @@ cd ${C_SERVICE}
 docker build -t rkp/customer-service:1.0 .
 cd ${C_SERVICE}/src/main/resources
 
-# deploy configmap
-kubectl apply -f customer-mysql-configmap.yml
-kubectl get configmap
-# Create credentials
-kubectl apply -f customer-mysqldb-credentials.yml
-kubectl apply -f customer-mysqldb-root-credentials.yml
-#kubectl get secrets
-
-#3. deploy mysql
-kubectl apply -f customer-mysql-deployment.yml
-#kubectl describe pods <pod id>
 #kubectl get deployments
 #4. Deploy customer service 
 kubectl apply -f customer-service-deployment.yml
